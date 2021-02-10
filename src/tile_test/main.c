960215cbd5f301587df3afa352ab7d5140b8911b
Philip Gonzalez <pgonzalez115@gmail.com>
2021-02-10 15:15:12 -0700

    Use preprocessor instead of constants

--- src/tile_test/main.c
+++ src/tile_test/main.c
@@ -4,13 +4,21 @@
 #include "MyTileMap.c"
 #include "Smile.c"
 
+#define TILE_SIZE 8
+#define SCREEN_WIDTH 160
+#define SCREEN_HEIGHT 144
+#define GAME_FLOOR (SCREEN_HEIGHT - 16)
+#define PAST_SKY ((TILE_SIZE * 2) - 1)
+#define PAST_CLOUDS ((TILE_SIZE * 5) - 1)
+#define PAST_PYRAMIDS ((TILE_SIZE * 15) - 1)
+
 typedef unsigned char UINT8;
 typedef char INT8;
 
-const UINT8 TILE_SIZE = 8;
 UINT8 scroll_sky, scroll_clouds, scroll_pyramids, scroll_sidewalk;
 UINT8 cycle;
-UINT8 player_pos[] = {80, 144};
+
+UINT8 player_pos[] = {80 - 8, GAME_FLOOR};
 
 
 void parallaxScroll()
@@ -18,19 +26,19 @@ void parallaxScroll()
     if (LYC_REG == 0x00)
     {
         move_bkg(scroll_sky, 0);
-        LYC_REG = TILE_SIZE * 2;
+        LYC_REG = PAST_SKY;
     }
-    else if (LYC_REG == (TILE_SIZE * 2))
+    else if (LYC_REG == PAST_SKY)
     {
         move_bkg(scroll_clouds, 0);
-        LYC_REG = TILE_SIZE * 5;
+        LYC_REG = PAST_CLOUDS;
     }
-    else if (LYC_REG == (TILE_SIZE * 5))
+    else if (LYC_REG == PAST_CLOUDS)
     {
         move_bkg(scroll_pyramids, 0);
-        LYC_REG = TILE_SIZE * 15 - 2;
+        LYC_REG = PAST_PYRAMIDS;
     }
-    else if (LYC_REG == (TILE_SIZE * 15 - 2))
+    else if (LYC_REG == PAST_PYRAMIDS)
     {
         move_bkg(scroll_sidewalk, 0);
         LYC_REG = 0x00;
@@ -43,6 +51,13 @@ void moveBackground(const UINT8 i, const INT8 direction)
     scroll_clouds += (i % 3 == 0) ? (1 * direction) : 0;
     scroll_pyramids += (i % 4 == 0) ? (1 * direction) : 0;
     scroll_sidewalk += (1 * direction);
+}
+
+inline void movePlayer()
+{
+    // The move_sprite function offests the x value by 8
+    // and the y value by 16
+    move_sprite(0, player_pos[0] + 8, player_pos[1] + 16);
 }
 
 void handleInput(const UINT8 cycle)
@@ -64,7 +79,6 @@ void handleInput(const UINT8 cycle)
     {
         player_pos[1]++;
     }
-    move_sprite(0, player_pos[0], player_pos[1]);
 }
 
 void init()
@@ -99,6 +113,7 @@ void main(void)
     while(1)
     {
         handleInput(cycle);
+        movePlayer();
         ++cycle;
         if (cycle == 12) { cycle = 0; } // Used to move background in fractions of steps
         wait_vbl_done();
